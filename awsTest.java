ef8ed4452240e79cc9bdfb5a3e114ed7ef3fb9e8
adover134 <adfsfsf@naver.com>
2022-12-08 18:58:05 +0900

    오류 수정

--- awsTest.java
+++ awsTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a0566a81cf6a2916ba71a4990d4c06114729fcf6f86c42c995ddbe8bf470df51
-size 17054
+oid sha256:171af7addfa98a3721661e2bb85955e1923572d812353913a40556dcbd11d724
+size 18809
